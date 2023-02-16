9fd314ef68fcda19632d777bdf7ca568cc144202
Kevin Koech <kevinkoech@proton.me>
2023-02-16 10:18:41 -0800

    First use of puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main function- Entry point
+ *
+ * and return:0 means success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
