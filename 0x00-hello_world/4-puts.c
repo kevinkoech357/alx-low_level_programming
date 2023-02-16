da49182ed9f6a6432f3d2d0b0237352f92edf702
Kevin Koech <kevinkoech@proton.me>
2023-02-16 10:27:27 -0800

    betty?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main function- Entry point
+ * main - Entry point
  *
- * and return:0 means success
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
