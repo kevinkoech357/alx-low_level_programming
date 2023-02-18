33bc6e80a28d1392012ce582b50a637412f06549
Kevin Koech <kevinkoech@proton.me>
2023-02-17 23:21:04 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,8 @@ int main(void)
 	for (n = 0; n < 10; n++)
 		putchar(n % 10 + '0');
 		if (n != 9)
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 	putchar('\n');
 	return (0);
 }
