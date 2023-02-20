ed8083021a600412d54747147be7c540b284a361
Kevin Koech <kevinkoech@proton.me>
2023-02-19 23:34:39 -0800

    base10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,17 @@
 
 /**
  * main - Entry point
- *
+ * prints base 10 numbers
+ * in ascending order
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int n;
+	int base10;
 
-	for (n = 0; n < 10; n++)
-		printf("%d", n);
+	for (base10 = 0; base10 < 10; base10++)
+		printf("%d", base10);
 	putchar('\n');
 	return (0);
 }
