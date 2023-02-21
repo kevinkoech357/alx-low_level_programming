fa6e4c40b8b62aa048ed0ed5117a0ccb94ae2f8c
Kevin Koech <kevinkoech@proton.me>
2023-02-21 01:35:02 -0800

    prints _putchar onscreen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints _putchar followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
