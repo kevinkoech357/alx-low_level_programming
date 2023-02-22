6252f1e1c4b7566dc89f396dc5cd1477dcc33575
Kevin Koech <kevinkoech@proton.me>
2023-02-22 03:12:44 -0800

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			printf("%d ", i);
 			if (i != 98)
 				printf(",");
 		}
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			printf("%d ", i);
 			if (i != 98)
 				printf(",");
 		}
@@ -34,13 +34,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i++)
 		{
-			printf("%d", i);
+			printf("%d ", i);
 			if (i != 98)
 				printf(",");
 		}
 	}
 	else if (n == 98)
 	{
-		printf("%d", n);
+		printf("%di ", n);
 	}
 }
