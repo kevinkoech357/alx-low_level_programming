72cd0ddf387d1b9f90d6e8bd37f361ed897e0f29
Kevin Koech <kevinkoech@proton.me>
2023-02-23 02:01:59 -0800

    bugless code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
 
-	else if (a > c && c > b)
+	else if (a > b && c > a)
 	{
-		largest = a;
+		largest = c;
 	}
 
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-
-	else if (b > c && c > a)
+	else if (b > c)
 	{
 		largest = b;
 	}
