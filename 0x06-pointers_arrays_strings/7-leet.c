68f9d00e054a790d21aed6564d36dcb9d184494c
Kevin Koech <kevinkoech@proton.me>
2023-03-03 10:10:57 -0800

    1337 style encoded string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,30 @@
 #include "main.h"
 
 /**
+ * *leet - fxn that encodes a string to 1337
  *
+ * @n: string
  *
- *
- *
- *
+ * Return: encoded string
  */
 
-char *leet(char *)
+char *leet(char *n)
 {
+	int i, j;
 
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+
+	return (n);
 }
