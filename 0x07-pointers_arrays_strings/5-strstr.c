6030440354bf4b87ebee046c92947e854cbf74f4
Kevin Koech <kevinkoech@proton.me>
2023-03-06 11:03:30 -0800

    strstr use to locate a string in a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * *_strstr - fxn that locates a substring
+ *
+ * @haystack: string to be searched
+ *
+ * @needle: string involved
+ *
+ * Return: poiner
+ * or NULL if string aint found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	i = j = 0;
+	if (needle[i] == '\0')
+	{
+		return (haystack);
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+	}
+
+	return ('\0');
+}
