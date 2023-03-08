1879c6af5ef1ff9f0fb093a9f621ad3b28e7868e
Kevin Koech <kevinkoech@proton.me>
2023-03-08 06:12:44 -0800

    betty correction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - fxn that returns value of 
+ * _pow_recursion - fxn that returns value of
  * x raised to the power of y
  *
  * @x: base number
@@ -19,7 +19,7 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	
+
 	if (y == 0)
 	{
 		return (1);
