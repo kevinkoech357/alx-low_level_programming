a3c43d19987224d980e17fa43a759f6da5edf274
Kevin Koech <kevinkoech@proton.me>
2023-03-08 05:46:12 -0800

    fxn that returns value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - fxn that returns value of 
+ * x raised to the power of y
+ *
+ * @x: base number
+ *
+ * @y: power value
+ *
+ * Return: value of x raised to
+ * power of y if y > 0
+ * and -1 if  < 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
