fb2f7e4601a4498c9440c013b05112fa3999fc69
Kevin Koech <kevinkoech@proton.me>
2023-03-10 06:48:23 -0800

    argc use to print number of command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints number of command line arguments
+ *
+ * @argc: number of command line arguments
+ *
+ * @argv: array of character commands
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
