0232fd05f7c76decc4d799234e687d8d569e1d40
Kevin Koech <kevinkoech@proton.me>
2023-03-10 06:44:02 -0800

    prints name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints name of the program
+ *
+ * @argc: number of command line arguments
+ *
+ * @argv: array of character commands
+ *
+ * Return: 0
+ *
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
