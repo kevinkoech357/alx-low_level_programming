7ce2b367c530bcacb739a5bbba0b2d6acc2baf49
Kevin Koech <kevinkoech@proton.me>
2023-03-15 00:31:13 -0700

    correction for compilation

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,6 +8,12 @@
 
 void _puts(char *s)
 {
+	int index;
 
+	for (index = 0; *(s + index) != '\0'; index++)
+	{
+		_putchar(*(s + index));
+	}
+	_putchar('\n');
 }
 
