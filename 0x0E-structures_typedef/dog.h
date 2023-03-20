0cfb13fdeece5e38de8f9ee55f010aa212eefcfd
Kevin Koech <kevinkoech@proton.me>
2023-03-20 13:35:04 +0300

    use of struct to store name,age and owner of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - declares a struct name dog
+ *
+ * @name: name of the dog
+ *
+ * @age: dog's age
+ *
+ * @owner: name of the owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
