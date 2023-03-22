dd65ab7d54d03df06c5a9ba7ce3a61add6f5ef6f
Kevin Koech <kevinkoech@proton.me>
2023-03-22 14:04:39 +0300

    added a requirement

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -19,8 +19,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
