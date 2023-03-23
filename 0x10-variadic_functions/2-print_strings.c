32d59273b6fe7c5f084f1e9336f20a1a61a71de1
Kevin Koech <kevinkoech@proton.me>
2023-03-23 14:26:19 +0300

    correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,11 +18,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
-	if (separator == NULL)
-	{
-		printf("nil");
-	}
-
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
