2f6b2d69a1efb088a24a5fe9454ef07decb05f7c
Kevin Koech <kevinkoech@proton.me>
2023-03-30 17:24:33 +0300

    correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,14 +19,14 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	new_node = malloc(sizeof(list_t));
-
 	if (str == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
@@ -34,7 +34,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	
+
 	new_node->len = strlen(str);
 	new_node->next = *head;
 
