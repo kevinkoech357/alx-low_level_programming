29be8df1ca7a3631c9317cb033e699437863c26d
Kevin Koech <kevinkoech@proton.me>
2023-03-31 16:28:31 +0300

    prints line specified before main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
+void __attribute__((constructor)) before(void);
+
 /**
-* __attribute__ - keyword that allows specification
-* of additional attributes of a fxn
-*
-* Description - prints line before main fxn is executed
+* before - prints line before main fxn is executed
 *
 * Return: void
 */
 
-void __attribute__((constructor)) before main()
+void __attribute__((constructor)) before(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
