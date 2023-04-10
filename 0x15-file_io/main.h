98ab94d9d0ce378f91a6495f725c2d9f2dd82ba4
Kevin Koech <kevinkoech@proton.me>
2023-04-10 10:48:44 +0300

    added header files required

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* header file */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 /* Prototypes */
 
 ssize_t read_textfile(const char *filename, size_t letters);
