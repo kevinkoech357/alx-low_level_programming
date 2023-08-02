7c0bf8a186e5aa04064e619f3350e0e981fd5a69
Kevin Koech <kevinkoech@proton.me>
2023-08-02 10:57:22 +0300

    pycodestyle correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,14 +16,16 @@ def island_perimeter(grid):
     Returns:
         int: Perimeter of island
 
-    Requirements:      
-        0 represents a water zone
-        1 represents a land zone
-        One cell is a square with side length 1
-        Grid cells are connected horizontally/vertically (not diagonally).
-        Grid is rectangular, width and height don’t exceed 100
-        Grid is completely surrounded by water, and there is one island (or nothing).
-        The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+    Requirements:
+        -0 represents a water zone
+        -1 represents a land zone
+        -One cell is a square with side length 1
+        -Grid cells are connected horizontally/vertically (not diagonally).
+        -Grid is rectangular, width and height don’t exceed 100
+        -Grid is completely surrounded by water,
+        and there is one island (or nothing).
+        -The island doesn’t have “lakes”
+        (water inside that isn’t connected to the water around the island).
     """
 
     if not grid or not grid[0]:
