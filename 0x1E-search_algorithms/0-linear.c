8c5ac2d0af1b23aa4994c007d0b01160739f797c
Kevin Koech <kevinkoech@proton.me>
2023-10-11 22:23:53 +0300

    correction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -25,7 +25,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 		{
-			printf("Value checked array[%lu] = [ %d]\n", i, array[i]);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			return (i);
 		}
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
